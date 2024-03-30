fc4a1b785f08e2806259d7d2c6eea2c5b6e67215
faisalch10 <faisal.chaudhry1133@gmail.com>
2024-03-31 02:43:19 +0500

    recaptha security check added

--- login.php
+++ login.php
@@ -1,44 +1,63 @@
-<?php include_once("./includes/connection.php") ?>
-<?php include_once("./includes/session.php"); ?>
-<?php include_once("./includes/helpers.php"); ?>
-<?php include_once("./includes/header.php"); ?>
+<?php include_once ("./includes/connection.php") ?>
+<?php include_once ("./includes/session.php"); ?>
+<?php include_once ("./includes/helpers.php"); ?>
+<?php include_once ("./includes/header.php"); ?>
 
 
 <?php
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv as Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$siteKey = $_ENV['RECAPTHA_SITE_KEY'];
+$secret = $_ENV['RECAPTHA_SECRET_KEY'];
+$lang = $_ENV['RECAPTHA_WIDGET_LANGUAGE'];
+
+
 if (isset($_POST["submit"])) {
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  if (empty($email) || empty($password)) {
-    echo '<script type="text/javascript">toastr.error("Please provide email and password. Both fields are mandatory")</script>';
-  } else {
-
-    $query = "SELECT * FROM users WHERE email = '$email'";
-    $execute = mysqli_query($conn, $query);
-
-    // mysqli_fetch_assoc() return associative array
-    if ($is_user_found = mysqli_fetch_assoc($execute)) {
-      if (password_verify($password, $is_user_found['password'])) {
-
-        $_SESSION['user_type'] = $is_user_found['user_type'];
-        $_SESSION['name'] = $is_user_found['name'];
-        $_SESSION['id'] = $is_user_found['id'];
-
-        if ($is_user_found['user_type'] === 'admin') {
-          // redirect_to('../admin/admin-dashboard.php');
-          echo "Please show admin page";
-        } else {
-          echo '<script type="text/javascript">toastr.success("You logged in successfully. Please wait while we redirecting you")</script>';
-          redirect_to(1.5, 'index.php');
-        }
-      } else {
-        echo '<script type="text/javascript">toastr.error("Invalid Password")</script>';
-      }
+  if (isset($_POST['g-recaptcha-response'])) {
+    $recaptcha = new \ReCaptcha\ReCaptcha($secret);
+    $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+    if (!$resp->isSuccess()) {
+      echo '<script type="text/javascript">toastr.error("Please complete the reCAPTCHA to proceed and verify you\'re human.")</script>';
     } else {
-      echo '<script type="text/javascript">toastr.error("User not exists with this email")</script>';
+
+      if (empty($email) || empty($password)) {
+        echo '<script type="text/javascript">toastr.error("Please provide email and password. Both fields are mandatory")</script>';
+      } else {
+
+        $query = "SELECT * FROM users WHERE email = '$email'";
+        $execute = mysqli_query($conn, $query);
+
+        // mysqli_fetch_assoc() return associative array
+        if ($is_user_found = mysqli_fetch_assoc($execute)) {
+          if (password_verify($password, $is_user_found['password'])) {
+
+            $_SESSION['user_type'] = $is_user_found['user_type'];
+            $_SESSION['name'] = $is_user_found['name'];
+            $_SESSION['id'] = $is_user_found['id'];
+
+            if ($is_user_found['user_type'] === 'admin') {
+              // redirect_to('../admin/admin-dashboard.php');
+              echo "Please show admin page";
+            } else {
+              echo '<script type="text/javascript">toastr.success("You logged in successfully. Please wait while we redirecting you")</script>';
+              redirect_to(1.5, 'index.php');
+            }
+          } else {
+            echo '<script type="text/javascript">toastr.error("Invalid Password")</script>';
+          }
+        } else {
+          echo '<script type="text/javascript">toastr.error("User not exists with this email")</script>';
+        }
+      }
     }
   }
-
 }
 
 ?>
@@ -57,14 +76,20 @@ if (isset($_POST["submit"])) {
         <div class="input-group mb-3">
           <input name="password" type="password" class="form-control" placeholder="Enter your password" />
         </div>
+
+        <div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>"></div>
+        <script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang; ?>">
+        </script>
         <input name="submit" type="submit" value="Login" class="btn btn-dark btn-block w-100 mt-3 mb-3" />
+
       </form>
 
       <div class="d-flex justify-content-end">
         Don't have an account? <a href="#">&nbsp;Register</a>
       </div>
+
     </div>
   </div>
 </div>
 
-<?php include_once("./includes/footer.php"); ?>
+<?php include_once ("./includes/footer.php"); ?>
--- register.php
+++ register.php
@@ -5,6 +5,17 @@
 
 <?php
 
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv as Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$siteKey = $_ENV['RECAPTHA_SITE_KEY'];
+$secret = $_ENV['RECAPTHA_SECRET_KEY'];
+$lang = $_ENV['RECAPTHA_WIDGET_LANGUAGE'];
+
+
 if (isset($_POST["submit"])) {
 
   $name = $_POST["name"];
@@ -12,40 +23,50 @@ if (isset($_POST["submit"])) {
   $email = $_POST["email"];
   $password = $_POST["password"];
 
-  if (empty($name) || empty($username) || empty($email) || empty($password)) {
-    echo '<script type="text/javascript">toastr.error("Please fill all the fields. All fields are mandatory")</script>';
-  } else {
 
-    // CHECKING IF USER ALREADY EXIST WITH EMAIL OR NOT
-    $is_user_exists_query = "SELECT * FROM users WHERE email='$email'";
-    $execute_is_user_exists_query = mysqli_query($conn, $is_user_exists_query);
-
-    // if below function mysqli_num_rows returns 1 means user already exist with same email
-    // mysqli_num_rows($execute_is_user_exists_query);
-
-    if (mysqli_num_rows($execute_is_user_exists_query) > 0) {
-      $row = mysqli_fetch_assoc($execute_is_user_exists_query);
-      if (ctype_lower($email) === ctype_lower($row['email'])) {
-        echo '<script type="text/javascript">toastr.error("User already exists with same email")</script>';
-      }
+  if (isset($_POST['g-recaptcha-response'])) {
+    $recaptcha = new \ReCaptcha\ReCaptcha($secret);
+    $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+    if (!$resp->isSuccess()) {
+      echo '<script type="text/javascript">toastr.error("Please complete the reCAPTCHA to proceed and verify you\'re human.")</script>';
     } else {
 
-      // Hashing password
-      $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-      $query = "INSERT INTO users(name, username, email, password, user_type, profile_image) VALUES('$name', '$username', '$email', '$hashed_password', 'user', 'default.png')";
-      $result = mysqli_query($conn, $query);
-
-      // $result -> return 1 if everything went ok otherwise 0
-      if ($result) {
-        echo '<script type="text/javascript">toastr.success("User registered successfully!")</script>';
-        redirect_to(2, 'login.php');
+      if (empty($name) || empty($username) || empty($email) || empty($password)) {
+        echo '<script type="text/javascript">toastr.error("Please fill all the fields. All fields are mandatory")</script>';
       } else {
-        echo '<script type="text/javascript">toastr.error("Something went wrong!")</script>';
+
+        // CHECKING IF USER ALREADY EXIST WITH EMAIL OR NOT
+        $is_user_exists_query = "SELECT * FROM users WHERE email='$email'";
+        $execute_is_user_exists_query = mysqli_query($conn, $is_user_exists_query);
+
+        // if below function mysqli_num_rows returns 1 means user already exist with same email
+        // mysqli_num_rows($execute_is_user_exists_query);
+
+        if (mysqli_num_rows($execute_is_user_exists_query) > 0) {
+          $row = mysqli_fetch_assoc($execute_is_user_exists_query);
+          if (ctype_lower($email) === ctype_lower($row['email'])) {
+            echo '<script type="text/javascript">toastr.error("User already exists with same email")</script>';
+          }
+        } else {
+
+          // Hashing password
+          $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+          $query = "INSERT INTO users(name, username, email, password, user_type, profile_image) VALUES('$name', '$username', '$email', '$hashed_password', 'user', 'default.png')";
+          $result = mysqli_query($conn, $query);
+
+          // $result -> return 1 if everything went ok otherwise 0
+          if ($result) {
+            echo '<script type="text/javascript">toastr.success("User registered successfully!")</script>';
+            redirect_to(2, 'login.php');
+          } else {
+            echo '<script type="text/javascript">toastr.error("Something went wrong!")</script>';
+          }
+        }
       }
+
     }
   }
-
 
 }
 
@@ -70,6 +91,11 @@ if (isset($_POST["submit"])) {
         <div class="input-group mb-3">
           <input name="password" type="password" class="form-control" placeholder="Enter your password" />
         </div>
+
+        <div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>"></div>
+        <script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang; ?>">
+        </script>
+
         <input name="submit" type="submit" value="Register" class="btn btn-dark btn-block w-100 mt-3 mb-3" />
 
 
